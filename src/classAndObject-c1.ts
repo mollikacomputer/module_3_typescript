455f0bd577d4b1d7f2bc373c150b75a52ef104d3
Ranjit Kumar Mandal <94553716+mollikacomputer@users.noreply.github.com>
2026-05-08 12:46:03 +0600

    class constructor

--- src/classAndObject-c1.ts
+++ src/classAndObject-c1.ts
@@ -1,2 +1,35 @@
-// module 3.1
-// class and object
+// // module 3.1
+// // class and object
+// // 1. system details
+// class Animal{
+//     name:string;
+//     species:string;
+//     sound:string;
+//     constructor(name:string, species:string, sound:string){
+//         this.name = name,
+//         this.species = species,
+//         this.sound = sound
+//     }
+//     // method for function
+//     makeSound(){
+//         console.log(`${this.name} is making ${this.sound}`);
+//     }
+// }
+// const dog = new Animal("dog dog dogesh", "Dog", "Ghew Ghew");
+// const cat = new Animal ("Cat Vai", "Cat", "mew mew");
+// cat.makeSound();
+// dog.makeSound();
+
+//2. system parameter properties
+not working this code
+class Animal{
+    constructor( public name:string, public species:string, public sound:string){
+    }
+    // method for function
+    makeSound=()=>{
+        console.log(`${this.name} is making ${this.sound}`);
+    }
+}
+const cat = new Animal ("Cat Vai", "Cat", "mew mew");
+cat.makeSound();
+
